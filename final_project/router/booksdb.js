f3189495513246820c7a9609586458a06d827d6f
Ali Reza <34375633+AliRezaBmeDu@users.noreply.github.com>
2023-05-07 00:49:06 +0600

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,5 +1,5 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"author": "Chinua Achebe","title": "Things Fall Apart","type": "tragic novel", "subject":"pre-colonial life in Nigeria and invasion by the Europeans"} },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
       3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
       4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
